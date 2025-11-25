d7906fe87a50ca4d772d32d2b8eec9c1e6d8123b
Jorge Padierne <jorgebolanos535@gmail.com>
2025-11-25 17:34:44 +0000

    Initial commit of the project

---
+++ windows/tocororo/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::tocororo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::tocororo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
